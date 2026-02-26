b82ec63a1795e663035005f822e4db2044e45adf
CodeWithAkash <iamakazz2004@gmail.com>
2026-02-26 19:45:21 +0530

    modified config.py

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
     REDIRECT_URI = os.getenv("REDIRECT_URI")
