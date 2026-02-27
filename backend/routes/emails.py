f488a38f81656c5acbb7eebe4e00af62f8d3e65a
CodeWithAkash <iamakazz2004@gmail.com>
2026-02-27 20:13:49 +0530

    modified  emails.py

--- backend/routes/emails.py
+++ backend/routes/emails.py
@@ -43,7 +43,7 @@ def get_emails():
                     'cached': True
                 })
 
-        messages = gmail_service.fetch_job_emails(max_results=20)
+        messages = gmail_service.fetch_job_emails(max_results=15)
 
         emails_data = []
 
